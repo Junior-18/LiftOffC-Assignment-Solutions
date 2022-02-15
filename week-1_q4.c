84123e3ab61953c8334f6259330b8d2f29f7113b
Junior-18 <98383048+Junior-18@users.noreply.github.com>
2022-02-16 00:01:25 +0530

    Update week-1_q4.c

--- week-1_q4.c
+++ week-1_q4.c
@@ -2,16 +2,48 @@
 perform all arithmetic operations.*/
 
 #include<stdio.h>
-int main(){ 
+
+int main(){
     int a, b;
-    float c;
-    printf("Enter two numbers with a space in between : \n");
+    char op;
+    printf("Enter the two operands : \n");
     scanf("%d %d", &a, &b);
-    c = (float)a/b;
-    printf("The sum of the two numbers is : %d \n", (a+b));
-    printf("The substraction results : %d \n", (a-b));
-    printf("The multiplication results : %d \n", (a*b));
-    printf("The division results : %f \n", c);
-    printf("The modulo is : %d \n", (a%b));
+    printf("Enter the operator : \n");
+    scanf("%c", &op);
+    scanf("%c", &op);
+    switch(op){
+        case '+':
+        printf("%d + %d = %d", a, b, a+b);
+        break;
+        case '-':
+        printf("%d - %d = %d", a, b, a-b);
+        break;
+        case '*':
+        printf("%d * %d = %d", a, b, a*b);
+        break;
+        case '/':
+        printf("%d / %d = %f", a, b, (float)a/b);
+        break;
+        default:
+        printf("Invalid Input.");
+        break;
+    }
     return 0;
 }
+
+
+
+// #include<stdio.h>
+// int main(){ 
+//     int a, b;
+//     float c;
+//     printf("Enter two numbers with a space in between : \n");
+//     scanf("%d %d", &a, &b);
+//     c = (float)a/b;
+//     printf("The sum of the two numbers is : %d \n", (a+b));
+//     printf("The substraction results : %d \n", (a-b));
+//     printf("The multiplication results : %d \n", (a*b));
+//     printf("The division results : %f \n", c);
+//     printf("The modulo is : %d \n", (a%b));
+//     return 0;
+// }
